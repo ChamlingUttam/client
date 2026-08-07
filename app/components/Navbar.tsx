d28e29e84cefb6ea9fe929c834efb5c3791d2dc1
ChamlngUttam <uttamchamling51@gmail.com>
2026-08-07 11:28:34 +0545

    work with the navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import React from 'react'
+import SearchBar from './SearchBar'
+import { Bell, Home, ShoppingCart } from 'lucide-react'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex justify-between items-center border-b border-gray-200 pb-4'>
+        {/*left */}
+        <Link href={"/"} className='flex items-center'>
+        <img src="/logo.png" alt="logo" width={36} height={36} className='w-6 h-6 md:w-9 md:h-9' />
+        <p className='text-md font-medium tracking-wider md:block hidden'>STORE.</p>
+        </Link>
+
+        {/* right*/}
+        <div className='flex gap-2 items-center'>
+            <SearchBar/>
+            <Link href={"/"}>
+            <Home className='w-4 h-4 text-gray-400' />
+            </Link>
+            <Bell className='w-4 h-4 text-gray-400'/>
+            <ShoppingCart className='w-4 h-4 text-gray-400'/>
+            <Link href={"/login"}>
+            Sign in
+            </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
+import Fotter from "./components/Fotter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,13 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="mx-auto p-4  sm:max-w-xl md:w-2xl lg:max-3xl xl:max-w-6xl">
+          <Navbar/>
+        {children}
+        <Fotter/>
+        </div>
+        </body>
     </html>
   );
 }
