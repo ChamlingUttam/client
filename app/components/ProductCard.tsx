f4c1885b2a4e594b52e398fe60d80a5f7cdf0729
ChamlngUttam <uttamchamling51@gmail.com>
2026-08-14 15:27:38 +0545

    work with the catergories of hero section

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ProductType } from '../types'
+
+const ProductCard = ({product}:{product:ProductType}) => {
+  return (
+    <div>
+      productscard
+    </div>
+  )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import Image from "next/image"
+import ProductList from "./components/ProductList"
 
 
 const page = () => {
   return (
     <div className="">
-      home page
-      
+    <div className="relative aspect-[3/1] mb-12">
+      <Image src="/featured.png" alt="content" fill/>
+    </div>
+    <ProductList/>
     </div>
   )
 }
